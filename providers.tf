89144f399e1658d414a1e144dfe9ae656badf6e5
Afzal Ahmad <afzalfaizi@gmail.com>
2020-06-01 00:42:20 +0530

    Providers

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "${var.region}"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "javahome-tf-20201234567"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "javahome-tf"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "javahome-tf-20201234567"
+#     key    = "terraform.tfstate"
+#     region = "ap-south-1"
+#     dynamodb_table = "javahome-tf"
+#   }
+# }
